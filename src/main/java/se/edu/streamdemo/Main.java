e843856099dc5c05ecef2aa31c0124547403c079
RexYong <rexyyong@gmail.com>
2024-02-28 11:21:49 +0800

    Consume new methods in main

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -14,11 +14,14 @@ public class Main {
 
         System.out.println("Printing all data ...");
         printAllData(tasksData);
+        printAllDataUsingStreams(tasksData);
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
+        printDeadlinesUsingStreams(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines (using Streams): " + countDeadlinesUsingStreams(tasksData));
 
     }
 
